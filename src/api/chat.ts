0828cd2fcd132fa976624a67d40e67ef332d2feb
Rover <rover.stargazer1@gmail.com>
2026-02-20 22:38:37 +0800

    feat 标记已读的接口

--- src/api/chat.ts
+++ src/api/chat.ts
@@ -70,13 +70,18 @@ export interface GetChatListResponse {
           unread_count?: number
     }[]
 }
+export interface SignReadResponse {
+    code: number,
+    msg: string,
+    data: null
+}
 //获取会话列表接口
 export function getChatList() {
-  return request.get<GetChatListResponse>('/api/v1/chat/list')
+  return request.get<GetChatListResponse>('/api/v1/messages/chats')
 }
 // 获取与特定用户的聊天历史记录
 export function getChatHistory(params: GetChatHistoryParams) {
-    return request.get<GetChatHistoryResponse>('/api/v1/chat/history', {
+    return request.get<GetChatHistoryResponse>('/api/v1/messages/history', {
         params: {
             target_id: params.target_id,
             page_num: params.page_num || 1,
@@ -87,4 +92,8 @@ export function getChatHistory(params: GetChatHistoryParams) {
 // 发送消息接口
 export function sendMessage(data: SendMessageRequest) {
     return request.post('/api/v1/messages', data)
+}
+// 标记消息为已读接口
+export function signRead(target_id: number) {
+    return request.post<SignReadResponse>('/api/v1/messages/read', { target_id })
 }
